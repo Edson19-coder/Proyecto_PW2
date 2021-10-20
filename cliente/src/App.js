b3a2ae68cdd4921270826be9df1bae9c087f85c0
Carlos Adrian Betancourt Samaniego <64230191+PainChip@users.noreply.github.com>
2021-10-20 08:04:14 -0500

    ABCProd, Busqueda, Carro, Login, Registro, Producto
    Creacion de componentes

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Carrito from './components/Carrito';
+import Busqueda from './components/Busqueda';
+import Producto from './components/Producto';
+import ABCProduct from './components/ABCProduct';
 
 function App() {
   return (
     
-    <Home />
+    <ABCProduct />
     
   );
 }
