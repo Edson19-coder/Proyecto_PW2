8b8bf1a249ceb1f2dcfec728456f7b73e55f9310
Carlos Adrian Betancourt Samaniego <64230191+PainChip@users.noreply.github.com>
2021-11-19 04:40:55 -0600

    Update search.js

--- cliente2/src/pages/search.js
+++ cliente2/src/pages/search.js
@@ -10,12 +10,13 @@ import { getSearchCategory } from "../api/SearchAPI";
 
 const Search = (props) => {
     let { category } = useParams();
+    const [searched, setsearch] = useState([]);
 
     useEffect(()=>{
         getSearchCategory(category,1)
             .then(res => {
                 console.log(res);
-                //setproduct(res.data["products"]);
+                //setsearch(res.data["products"]);
             })
             .catch(err => {
                 console.log(err);
@@ -27,16 +28,10 @@ const Search = (props) => {
             <Container id="" className="container-fluid">
                 <h4 id="title">Resultados</h4><label id="label">Resultados de busqueda</label>
                 <hr className="hr"></hr>
+                    {/* {products.map((item) => (
+                        <CardSearch key={item._id} id={item.productId} price={item.cost} name={item.name} description={item.description} img={`${GLOBAL.url}/get-image-prod/${item.image}`} ></CardSearch>
+                    ))} */}
                     <CardSearch id="1"  price="5000" name="Squid Game" img="https://cdn.vox-cdn.com/thumbor/2QvgYM5Z2w02Qb-bdauWMIAqmyk=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/49907377/gtx-1080-msi.0.jpg" />
-                    <CardSearch id="12" price="5001" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="13" price="5002" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="14" price="5003" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="15" price="5004" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="16" price="5005" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="17" price="5006" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="18" price="5007" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="19" price="5008" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
-                    <CardSearch id="10" price="5009" name="Squid Game" img="https://assets.rockpapershotgun.com/images/2019/07/AMD-Ryzen-5-2600-best-budget-gaming-cpu.jpg" />
                     <PaginacionNosotros></PaginacionNosotros>
             </Container>
         </Fragment>
